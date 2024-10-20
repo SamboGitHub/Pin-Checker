45ede01182a653254f68163aca8139c1cfa4017e
Sam Bell <sambo@asab.us>
2024-10-20 09:03:30 -0400

    got it working. stil need to fix \r and add timestamp

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,9 @@ s_Read
 states state;
 
 const int num_pins = 13;
-boolean pin_value[num_pins];
-boolean read_pin;
-boolean current_pin_value;
+int pin_value[num_pins];
+int read_pin;
+int current_pin_value;
 int i=0;
 
 
@@ -34,9 +34,9 @@ state = s_Read;
 for (i=0 ; (i<num_pins+1) ; i++ )
   {
   pinMode(i, INPUT);
-  printf("  %d  ",i % 10);
-  pin_value[i]=false;
-  digitalWrite(i, HIGH);  // Pull High Restance 
+  printf("  %d  ",i % 10); // write last digita 
+  pin_value[i]=0;
+  // digitalWrite(i, HIGH);  // Pull High Restance 
   }
    printf("\n");
 
@@ -49,8 +49,8 @@ void loop()
   case s_Read:
    
     state = s_NoChange;
-    read_pin= false;
-    current_pin_value=false;
+    read_pin= 0;
+    current_pin_value=0;
 
     for(i=0;(i<num_pins+1);i++)
     {
@@ -78,7 +78,7 @@ void loop()
     state = s_Read;
     break;
   case s_NoChange:
-    printf("\r");
+    printf("\n");
     
     state = s_Read;
     break;
